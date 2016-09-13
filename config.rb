b980c19224bc6be3b2969971485490814f9d2b67
Evgeny Ivanov <busforward@gmail.com>
2016-09-13 11:29:50 +0500

    Удалил папки /psd /.sass-cashe

--- config.rb
+++ config.rb
@@ -6,4 +6,5 @@ output_style = :expanded # by Compass.app
 relative_assets = false # by Compass.app 
 line_comments = false # by Compass.app 
 sass_options = {:debug_info=>false} # by Compass.app 
-sourcemap = false # by Compass.app 
+sourcemap = false # by Compass.app
+cache = false
